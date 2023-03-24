b57216ea5fc8f362ce66348d0a5dc59f4292093c
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-03-24 14:45:07 +0100

    Aggiunge classi indirizzi e parser

---
+++ Indirizzo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+
+{
+    public class Indirizzo
+    {
+        public string name;
+        public string surname;
+        public string street;
+        public string city;
+        public string province;
+        public string zipCode;
+
+        public Indirizzo(string name, string surname, string street, string city, string province, string zipCode)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.zipCode = zipCode;
+        }
+
+
+    }
+}
